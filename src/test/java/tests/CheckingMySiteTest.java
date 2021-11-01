2e1d0f637ef32a66bb833bdc1b6c6a50c8aa9c19
Ihar Malinouski <malinouski.private@gmail.com>
2021-11-01 22:02:29 +0300

    Update CheckingCheckboxesTest, DropdownTest

---
+++ src/test/java/tests/CheckingMySiteTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class CheckingMySiteTest {
+
+    @Test
+    public void checkingCheckboxesTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("file:///D:/AutomationTestHeroku/src/test/java/tests/test.html");
+    }
+
+
+}
